76000245fc3329589205498b2820b0437c301683
venikpapa <venikpapa@gmail.com>
2025-02-04 20:51:01 +0300

    add proposal

--- scripts/proposals/scripts/arbitrum/03_transfer_usdplus.ts
+++ scripts/proposals/scripts/arbitrum/03_transfer_usdplus.ts
@@ -5,11 +5,11 @@ import { getContract, Roles } from '../../../helpers/script-utils';
 async function getProposalItems(): Promise<ProposalItems> {
     let xusdToken: Contract = await getContract('XusdToken');
 
-    let newImpl = '';
-    let oldImpl = '0x3f2FeD6FB49Ddc76e4C5CE5738C86704567C4D87';
+    let newImpl = '0x26085bDc0230AA48AFE8ce3F5f007694f2E44D97';
+    let oldImpl = '0x53c905E4fbE64bd03c15CD16b330D2Cc20EcA4E5';
     const TO_ADDRESS = '0x9030D5C596d636eEFC8f0ad7b2788AE7E9ef3D46';
 
-    const transfers = [{ from: '0xfe193d41bf8ee691bec999bce0981b6d06c89394', amount: 1732.7 }];
+    const transfers = [{ from: '0xfe193d41bf8ee691bec999bce0981b6d06c89394', amount: 1732 }];
 
     let items: ProposalItem[] = [{ contract: xusdToken, methodName: 'upgradeTo', params: [newImpl] }];
 
@@ -18,13 +18,14 @@ async function getProposalItems(): Promise<ProposalItems> {
         items.push({
             contract: xusdToken,
             methodName: 'transferStuckTokens',
-            params: [transfer.from, TO_ADDRESS, BigInt(transfer.amount * 10 ** 18)],
+            params: [transfer.from, TO_ADDRESS, BigInt(transfer.amount * 10 ** 6)],
         });
     }
 
     // Add final upgrade back to old implementation
     items.push({ contract: xusdToken, methodName: 'upgradeTo', params: [oldImpl] });
 
+    console.log(items);
     return await addProposalItems(items);
 }
 
